0db99bb80618aa7489b5e4509e8d89b50fa2e304
Venkateshb007 <venkateshb698@gmail.com>
2023-12-01 16:43:11 +0530

    updated commit

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const axios = require('axios');
 const dotenv = require('dotenv');
 const rateLimit = require('express-rate-limit');
+const cors = require('cors');
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 let locations = [];
 let weatherCache = {};
@@ -69,100 +71,29 @@ app.delete('/locations/:location_id', (req, res) => {
     }
 });
 
-app.get('/weather/:location_id', async (req, res) => {
-    const location = getLocationById(req.params.location_id);
-    if (!location) {
-        res.status(404).json('Location Not Found');
-    } else {
-        try {
-            if (weatherCache[location.id] && isCacheValid(weatherCache[location.id].timeStamp)) {
-                res.json(weatherCache[location.id].data);
-            } else {
-                const weatherData = await fetchWeatherData(location.latitude, location.longitude);
+app.get('/weather/:location_name', async (req, res) => {
+    const locationName = req.params.location_name;
+    try {
+        const weatherData = await fetchWeatherDataByName(locationName);
 
-                weatherCache[location.id] = {
-                    data: weatherData,
-                    timeStamp: Date.now(),
-                };
-                res.json(weatherData);
-            }
-        } catch (error) {
-            console.error(error);
-            res.status(500).send('Error Fetching Weather Data');
-        }
+        res.json(weatherData);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error Fetching Weather Data');
     }
 });
 
-app.get('/history/:location_id', (req, res) => {
-    const location = getLocationById(req.params.location_id);
-    if (!location) {
-        res.status(404).send('Location Not Found');
-    } else {
-        const historicalData = getHistoricalData(location.id);
-        if (!historicalData.length) {
-            res.send('No Historical Data Available');
-        } else {
-            const summary = generateSummary(historicalData);
-            res.json({ historicalData, summary });
-        }
-    }
-});
-
-function getLocationById(locationId) {
-    return locations.find((loc) => loc.id === parseInt(locationId));
-}
-
-function getHistoricalData(locationId) {
-    // Assuming historicalData is a global variable or imported from another module
-    return historicalData.filter((data) => data.locationId === locationId);
-}
-
-function generateSummary(historicalData) {
-    if (historicalData.length === 0) {
-        return null;
-    }
-    const totalTemperature = historicalData.reduce((sum, data) => sum + data.temperature, 0);
-    const averageTemperature = totalTemperature / historicalData.length;
-
-    const totalHumidity = historicalData.reduce((sum, data) => sum + data.humidity, 0);
-    const averageHumidity = totalHumidity / historicalData.length;
-
-    const totalWindSpeed = historicalData.reduce((sum, data) => sum + data.windSpeed, 0);
-    const averageWindSpeed = totalWindSpeed / historicalData.length;
-
-    const maxTemperature = Math.max(...historicalData.map((data) => data.temperature));
-    const minTemperature = Math.min(...historicalData.map((data) => data.temperature));
-
-    const maxHumidity = Math.max(...historicalData.map((data) => data.humidity));
-    const minHumidity = Math.min(...historicalData.map((data) => data.humidity));
-
-    const maxWindSpeed = Math.max(...historicalData.map((data) => data.windSpeed));
-    const minWindSpeed = Math.min(...historicalData.map((data) => data.windSpeed));
-
-    return {
-        averageTemperature,
-        averageHumidity,
-        averageWindSpeed,
-        maxTemperature,
-        minTemperature,
-        maxHumidity,
-        minHumidity,
-        maxWindSpeed,
-        minWindSpeed,
-    };
-}
-
-async function fetchWeatherData(latitude, longitude) {
+async function fetchWeatherDataByName(locationName) {
     const apiKey = process.env.OPEN_WEATHER_MAP_API_KEY;
     try {
         const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather`, {
             params: {
-                lat: latitude,
-                lon: longitude,
+                q: locationName.replace(/\s/g, '+'),
                 appid: apiKey,
                 units: 'metric',
             },
         });
+
         const { main, wind } = response.data;
         return {
             temperature: main.temp,
@@ -173,11 +104,6 @@ async function fetchWeatherData(latitude, longitude) {
         console.error(error);
         throw new Error('Error fetching weather data from OpenWeatherMap');
     }
-}
-
-function isCacheValid(timeStamp) {
-    const cacheDuration = 60 * 60 * 1000;
-    return Date.now() - timeStamp <= cacheDuration;
 }
 
 const PORT = process.env.PORT || 3000;
